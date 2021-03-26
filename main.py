e4c11d0996baac149131fffe587c9a5fc3e663a0
vuchaev2015 <42405830+vuchaev2015@users.noreply.github.com>
2021-03-26 17:09:23 +0300

    Fix negative values
    Added min and max price
    Added buy_slave = True/False
    Fix of negative values when buying
    
    Fix by Yagus228 lolzteam

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import requests, json, schedule, threading, random, time
 print('Enter your authorization: ')
 authorization = str(input()).rstrip().lstrip()
 
-anticooldown = random.randint(1, 5) # Анти кд
+anticooldown = random.randint(0, 0) # Анти кд, настраивать можете сами.
 job_name = 'Вьетнам' # Имя работы
-min_price = 1 # Минимальная цена
+
+min_price = 40 # Минимальная цена
+max_price = 1000000 # Максимальная цена
+
+buy_slave = True # Покупать рабов?
 buy_fetter = True # Покупать оковы? 
 
 print(f'Job name: {job_name}')
-print(f'Minimal price: {min_price}')
+print(f'Min price: {min_price}, max price: {max_price}')
+print(f'Buy slave: {buy_slave}')
 print(f'Buy fetter: {buy_fetter}')
 
 def myProfile():
@@ -128,17 +133,20 @@ def findSlave(slaves):
             if int(myProfile()['me']['balance']) >= int(slave['fetter_price']):
                 if int(slave['fetter_to']) == 0:
                     if int(slave['price']) >= min_price:
-                        time.sleep(anticooldown)
-                        print(f'Buy: ' + str(slave['id']))
-                        buySlave(slave['id'])
-                        time.sleep(anticooldown)
-                        jobSlave(slave['id'])
-                        if buy_fetter == True:
-                           time.sleep(anticooldown)
-                           print('Buy fetter: ' + str(slave['id']))
-                           buyFetter(slave['id'])
+                        if int(slave['price']) <= max_price:
+                            time.sleep(anticooldown)
+                            if buy_slave == True:
+                               slaveid = str(slave['id']).replace('-','')
+                               print(f'Buy: ' + slaveid)
+                               buySlave(int(slaveid))
+                               time.sleep(anticooldown)
+                               jobSlave(int(slaveid))
+                               if buy_fetter == True:
+                                  time.sleep(anticooldown)
+                                  print('Buy fetter: ' + slaveid)
+                                  buyFetter(int(slaveid))
         if int(slave['slaves_count']) != 0:
-            print('findSlave -> ' + str(slave['id']))
+            print('findSlave -> ' + int(slaveid))
             findSlave(slave['slaves'])
 
 def Profile():
@@ -146,18 +154,20 @@ def Profile():
     me = myProfile()['me']
     if me['fetter_to'] != 0:
         if me['price'] <= me['balance']:
-            buySlave(me['id'])
+            if buy_slave == True:
+               buySlave(me['id'])
     me = myProfile()['me']
     slaves = myProfile()['slaves']
     for slave in slaves:
+        slaveid = str(slave['id']).replace('-','')
         print('Profile -> ' + str(slave['id']))
         if slave['job']['name'] != job_name:
             time.sleep(anticooldown)
-            jobSlave(slave['id'])
+            jobSlave(int(slaveid))
         if slave['profit_per_min'] * 60 * slave['fetter_hour'] > slave['fetter_price'] and me['balance'] >= slave['fetter_price'] and slave['fetter_to'] == 0:
             if buy_fetter == True:
                            time.sleep(anticooldown)
-                           buyFetter(slave['id'])
+                           buyFetter(int(slaveid))
 
 def ThreadProfile():
     my_thread = threading.Thread(target=Profile)
