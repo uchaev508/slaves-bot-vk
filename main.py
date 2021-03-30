a4ebfd538bea2938dbf8dba05e4cd829598aa6eb
vuchaev2015 <42405830+vuchaev2015@users.noreply.github.com>
2021-03-30 14:22:09 +0300

    Cleaned up the code from debris.
    Cleaned up the code from debris
    Reworked the config
    Now you can include bugs in the bot's telegram if you want
    All convenient and no bugs noticed :)

--- main.py
+++ main.py
@@ -7,28 +7,19 @@ with open("config.json", encoding='utf-8-sig') as f: # cfg
     config = json.load(f)
 
 authorization = str(config["authorization"]).rstrip().lstrip()
-windows_title = config["windows_title"] 
 
-telegram_notifications = config["telegram_notifications"] 
-telegram_user_id = config["telegram_user_id"]  
-telegram_bot_token = config["telegram_bot_token"]  
+"""Все настройки в Config.json"""
+telegram_user_id = config['telegram_settings']['telegram_user_id']
+telegram_bot_token = config['telegram_settings']['telegram_bot_token']  
 
-job_name = config["job_name"] 
-min_price = config["min_price"] 
-max_price = config["max_price"]
-buy_slave = config["buy_slave"]
-buy_fetter = config["buy_fetter"]
-upgrade_slave = config["upgrade_slave"]
+"""Вывод текущих настроек"""
+print(f'Windows title: ' + str(config["windows_title"]) + '\nTelegram notifications: '+ str(config['telegram_settings']['telegram_notification']) + '\nJob name: ' + str(config['general_settings']['job_name']) + '\nMin price: ' + str(config['general_settings']['min_price']) + ', max price: ' + str(config['general_settings']['max_price']) + '\nBuy slave: '+ str(config['general_settings']['buy_slave']) + '\nBuy fetter: ' + str(config['general_settings']['buy_fetter']) + '\nUpgrade slave: ' + str(config['general_settings']['upgrade_slave']))
+if config['telegram_settings']['telegram_notification'] == True:
+    print(f'Telegram notify error: Job_name: ' + str(config['telegram_settings']['telegram_notify_error']['job_name']) + ', Buy slave: ' + str(config['telegram_settings']['telegram_notify_error']['buy_slave']) + ', Buy fetter: ' + str(config['telegram_settings']['telegram_notify_error']['buy_fetter']) + ', Sell slave: ' + str(config['telegram_settings']['telegram_notify_error']['sale_slave']) + '\n')
+else:
+    print('\n')
 
-print(f'Windows title: {windows_title}')
-print(f'Telegram notifications: {telegram_notifications}')
-print(f'Job name: {job_name}')
-print(f'Min price: {min_price}, max price: {max_price}')
-print(f'Buy slave: {buy_slave}')
-print(f'Buy fetter: {buy_fetter}')
-print(f'Upgrade slave: {upgrade_slave}')
-print(f'')
-
+"""Получение информации о своем профиле"""
 def myProfile():
     global authorization
     url = 'https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/start'
@@ -45,6 +36,7 @@ def myProfile():
             break
     return response.json()
 
+"""Получение информации о профиле игрока"""
 def userProfile(user_id):
     url = 'https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/user?id=' + str(user_id)
     payload = {}
@@ -60,6 +52,7 @@ def userProfile(user_id):
             break
     return response.json()
 
+"""Получение информации о топерах"""
 def topUsers():
     url = 'https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/topUsers'
     payload = {}
@@ -72,11 +65,12 @@ def topUsers():
     response = requests.request('GET', url, headers=headers, data=payload)
     return response.json()
 
+"""Устройство рабов на работу"""
 def jobSlave(slave_id):
     global job_name
     url = 'https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/jobSlave'
     payload = json.dumps({'slave_id':slave_id, 
-     'name':job_name[randrange(0, len(job_name))]})
+     'name':config['general_settings']['job_name'][randrange(0, len(config['general_settings']['job_name']))]})
     headers = {'sec-ch-ua':'"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"', 
      'authorization':authorization, 
      'sec-ch-ua-mobile':'?0', 
@@ -87,19 +81,21 @@ def jobSlave(slave_id):
     response = requests.request('POST', url, headers=headers, data=payload)
     if response.status_code == 422:
         print(f'Error when installing the job, possibly cooldown. Slave: {slave_id}')
-        if telegram_notifications == True:
-            requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Error when installing the job, possibly cooldown. Slave: {slave_id}")
+        if config['telegram_settings']['telegram_notification'] == True:
+            if config['telegram_settings']['telegram_notify_error']['job_name'] == True:
+               requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Error when installing the job, possibly cooldown. Slave: {slave_id}")
     elif response.status_code == 200:
         print(f'Set job: {slave_id}. Name: ' + userProfile(slave_id)['job']['name'])
-        if telegram_notifications == True:
+        if config['telegram_settings']['telegram_notification'] == True:
             requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Set job: {slave_id}. Name: " + userProfile(slave_id)['job']['name'])
     else:
         print(f'Unknown error. Slave: {slave_id}')
-        if telegram_notifications == True:
+        if config['telegram_settings']['telegram_notification'] == True:
             requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Unknown error. Slave: {slave_id}")
 
     return response.json()
 
+"""Покупка оков"""
 def buyFetter(slave_id):
     url = 'https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buyFetter'
     payload = json.dumps({'slave_id': slave_id})
@@ -113,19 +109,21 @@ def buyFetter(slave_id):
     response = requests.request('POST', url, headers=headers, data=payload)
     if response.status_code == 422:
         print(f'Error when buying fetter, possibly a cooldown. Slave: {slave_id}')
-        if telegram_notifications == True:
-           requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Error when buying fetter, possibly a cooldown. Slave: {slave_id}")
+        if config['telegram_settings']['telegram_notification'] == True:
+            if config['telegram_settings']['telegram_notify_error']['buy_fetter'] == True:
+                requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Error when buying fetter, possibly a cooldown. Slave: {slave_id}")
     elif response.status_code == 200:
         print(f'Buy fetter: {slave_id}. Price: ' + str(int(userProfile(slave_id)['fetter_price'])))
-        if telegram_notifications == True:
-           requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Buy fetter: {slave_id}. Price: "+ str(int(userProfile(slave_id)['fetter_price'])))
+        if config['telegram_settings']['telegram_notification'] == True:
+            requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Buy fetter: {slave_id}. Price: "+ str(int(userProfile(slave_id)['fetter_price'])))
     else:
         print(f'Unknown error. Slave: {slave_id}')
-        if telegram_notifications == True:
+        if config['telegram_settings']['telegram_notification'] == True:
            requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Unknown error. Slave: {slave_id}")
    
     return response.json()
 
+"""Покупка рабов"""
 def buySlave(slave_id):
     url = 'https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave'
     payload = json.dumps({'slave_id': slave_id})
@@ -139,19 +137,21 @@ def buySlave(slave_id):
     response = requests.request('POST', url, headers=headers, data=payload)
     if response.status_code == 422:
         print(f'Error when buying slave, possibly a cooldown. Slave: {slave_id}')
-        if telegram_notifications == True:
-          requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Error when buying slave, possibly a cooldown. Slave: {slave_id}")
+        if config['telegram_settings']['telegram_notification'] == True:
+            if config['telegram_settings']['telegram_notify_error']['buy_slave'] == True:
+                requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Error when buying slave, possibly a cooldown. Slave: {slave_id}")
     elif response.status_code == 200:
         print(f'Buy: {slave_id}. Price: ' + str(int(userProfile(slave_id)['price']/1.49998088027))) 
-        if telegram_notifications == True:
-           requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Buy: {slave_id}. Price: " + str(int(userProfile(slave_id)['price']/1.5)))
+        if config['telegram_settings']['telegram_notification'] == True:
+            requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Buy: {slave_id}. Price: " + str(int(userProfile(slave_id)['price']/1.5)))
     else:
         print(f'Unknown error. Slave: {slave_id}')
-        if telegram_notifications == True:
+        if config['telegram_settings']['telegram_notification'] == True:
            requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Unknown error. Slave: {slave_id}")
 
     return response.json()
 
+"""Получение списка рабов"""
 def slaveList(user_id):
     url = 'https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/slaveList?id=' + str(user_id)
     payload = {}
@@ -164,6 +164,7 @@ def slaveList(user_id):
     response = requests.request('GET', url, headers=headers, data=payload)
     return response.json()['slaves']
 
+"""Продажа рабов"""
 def saleSlave(slave_id):
     url = 'https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/saleSlave'
     payload = json.dumps({'slave_id': slave_id})
@@ -175,41 +176,43 @@ def saleSlave(slave_id):
     response = requests.request('POST', url, headers=headers, data=payload)
     if response.status_code == 422:
         print(f'Error when selling slave, possibly a cooldown. Slave: {slave_id}')
-        if telegram_notifications == True:
-          requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Error when selling slave, possibly a cooldown. Slave: {slave_id}")
+        if config['telegram_settings']['telegram_notification'] == True:
+            if config['telegram_settings']['telegram_notify_error']['sale_slave'] == True:
+                requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Error when selling slave, possibly a cooldown. Slave: {slave_id}")
     elif response.status_code == 200:
-        print(f'Sell: {slave_id}. Price: ' + str(int(userProfile(slave_id)['sale_price']/1.49998088027)))
-        if telegram_notifications == True:
-           requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Sell: {slave_id}. Price: " + str(int(userProfile(slave_id)['sale_price']/1.49998088027)))
+        print(f'Sale: {slave_id}. Price: ' + str(int(userProfile(slave_id)['sale_price']/1.49998088027)))
+        if config['telegram_settings']['telegram_notification'] == True:
+            requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Sale: {slave_id}. Price: " + str(int(userProfile(slave_id)['sale_price']/1.49998088027)))
     else:
         print(f'Unknown error. Slave: {slave_id}')
-        if telegram_notifications == True:
+        if config['telegram_settings']['telegram_notification'] == True:
            requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Unknown error. Slave: {slave_id}")
 
     return response.json()
 
+"""Обновление оков и установка работ в своем профиле рабам"""
 def Profile():
     me = myProfile()['me']
     if me['fetter_to'] != 0:
         if me['balance'] >= me['price']:
-            if buy_slave == True:
-                if int(slave['price']) >= min_price:
-                        if int(slave['price']) <= max_price:
-                           sleep(randint(config["min_delay"],config["max_delay"]))
+            if str(int(config['general_settings']['buy_slave'])) == True:
+                if int(slave['price']) >= config['general_settings']['min_price']:
+                        if int(slave['price']) <= config['general_settings']['max_price']:
+                           sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
                            buySlave(int(str(slave['id'])))
     me = myProfile()['me']
     slaves = myProfile()['slaves']
     
-    sleep(randint(config["min_delay"],config["max_delay"]))
+    sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
 
     for slave in slaves:
         if slave['job']['name'] == '':
-            sleep(randint(config["min_delay"],config["max_delay"]))
+            sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
             jobSlave((slave['id']))
 
         if slave['fetter_price'] <= me['balance'] and slave['fetter_to'] == 0:
-            if buy_fetter == True:
-                           sleep(randint(config["min_delay"],config["max_delay"]))
+            if config['general_settings']['buy_fetter'] == True:
+                           sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
                            buyFetter(int(str(slave['id'])))
 
 threading.Thread(target=Profile).start()
@@ -217,46 +220,47 @@ threading.Thread(target=Profile).start()
 while True:
     try:
         me = myProfile()['me']
-        if windows_title == True:
+        if config['windows_title'] == True:
            os.system(f'title Your ID: ' + str(me['id']) +  ", slaves: " + str(me['slaves_count']) + ", balance: " + str(me['balance']) + ", profit: " + str(me['slaves_profit_per_min']) + "/per min")
     except Exception as e:
         print(f'Critical Error - {e}')
         input
     try:
         schedule.run_pending()
-        if buy_slave == True:
+        if config['general_settings']['buy_slave'] == True:
             randomid = randint(10000, 647000000)
             slave = userProfile(randomid)
             print('Slave: ' + str(randomid))
 
             if int(myProfile()['me']['balance']) >= int(slave['fetter_price']):
                 if int(slave['fetter_to']) == 0:
-                    if int(slave['price']) >= min_price:
-                        if int(slave['price']) <= max_price:
-                            sleep(randint(config["min_delay"],config["max_delay"]))
-                            if buy_slave == True:
-                                if upgrade_slave == True:
+                    if int(slave['price']) >= config['general_settings']['min_price']:
+                        if int(slave['price']) <= config['general_settings']['max_price']:
+                            sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
+                            if config['general_settings']['buy_slave'] == True:
+                                if config['general_settings']['upgrade_slave'] == True:
                                    if int(me["balance"]) >= 39214:
                                     buySlave(randomid)
                                     while int(userProfile(randomid)['price']/1.49998088027)  < 26151:
                                           saleSlave(randomid)
-                                          sleep(randint(config["min_delay"],config["max_delay"]))
+                                          sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
                                           buySlave(randomid)
                                           if int(userProfile(randomid)['price']/1.49998088027) >= 26151:
                                              print(f'Upgrade done. Slave: {randomid}')
-                                             if telegram_notifications == True:
+                                             if config['telegram_settings']['telegram_notification'] == True:
                                                 requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_user_id}&text=Upgrade done. Slave: {randomid}")
                                    else:
                                     buySlave(randomid)
-                                    sleep(randint(config["min_delay"],config["max_delay"]))
+                                    sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
                                 else:
-                                    sleep(randint(config["min_delay"],config["max_delay"]))
+                                    sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
                                     buySlave(randomid)
-                                sleep(randint(config["min_delay"],config["max_delay"]))
+                                sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
                                 jobSlave(randomid)
-                                if buy_fetter == True:
-                                    sleep(randint(config["min_delay"],config["max_delay"]))
-                                    buyFetter(randomid)
+                                if config['general_settings']['buy_fetter'] == True:
+                                    if slave['fetter_price'] <= me['balance'] and slave['fetter_to'] == 0:
+                                       sleep(randint(config['general_settings']['min_delay'],config['general_settings']['max_delay']))
+                                       buyFetter(randomid)
 
     except Exception as inst:
         try:
